32b8014f3a8d0ee3683e80699b888a9f0c21a1ea
chuka <22582292+Chuka-Obijiaku@users.noreply.github.com>
2023-10-19 15:36:08 -0500

    Complete the bare bones logic in javascript

--- javascript.js
+++ javascript.js
@@ -1,6 +1,40 @@
-function getComputerChoice() {
-    let possibleChoices = ['Rock', 'Paper', 'Scissors']
-    return possibleChoices[Math.floor(Math.random() * 3)];
+function getComputerChoice(possibleChoices) {
+    // Chooses random value from a list of strings
+    return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
 }
 
-console.log(getComputerChoice());
+function getWinner(player, computer) {
+    // returns true if player wins, returns False if player loses 
+    if (player  == "rock" && computer  == "scissor")  {
+        return true;
+    }
+    else if (player  == "paper" && computer  == "rock") {
+        return true;
+    }
+    else if (player  == "scissor" && computer  == "paper") {
+        return true;
+    }
+
+    return false;
+    
+}
+
+
+function play() {
+    const choices = ["Rock", "Paper", "Scissors"]
+    let compChoice = getComputerChoice(choices);
+    let playerChoice = prompt("Rock, Paper, or Scissors: ").trim();
+    let winner = getWinner(playerChoice, compChoice)
+    
+    if (winner) {
+        alert("You Win");
+    
+    }
+    else {
+        alert("You Lose");
+    }
+
+
+}
+
+play();
