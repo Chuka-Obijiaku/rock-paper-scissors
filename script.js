051ae6dc8fd964819711b12ef1e30756356f8098
chuka <22582292+Chuka-Obijiaku@users.noreply.github.com>
2023-10-20 03:35:00 -0500

    Add functionality to the butons in script.js
    used event listeners to listent to button click and run game()

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+
+// declare variables as buttons 
+
+const rockBtn = document.querySelector('#rock-btn');
+const paperBtn = document.querySelector('#paper-btn');
+const scissorsBtn = document.querySelector('#scissors-btn');
+
+
 function getComputerChoice(possibleChoices) {
     // Chooses random value from a list of strings and makes it lower case
-    return possibleChoices[Math.floor(Math.random() * possibleChoices.length)].toLowerCase();
+
+    return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
 }
 
 function getWinner(player, computer) {
     // returns true if player wins, returns False if player loses 
-    if (player  == "rock" && computer  == "scissor")  {
+
+    if (player  == "Rock" && computer  == "Scissor")  {
         return true;
     }
-    else if (player  == "paper" && computer  == "rock") {
+    else if (player  == "Paper" && computer  == "Rock") {
         return true;
     }
-    else if (player  == "scissors" && computer  == "paper") {
+    else if (player  == "Scissors" && computer  == "Paper") {
         return true;
     }
     else if (player == computer) {
@@ -22,9 +32,9 @@ function getWinner(player, computer) {
     
 }
 
-
 function playRound(player, comp) {
     // takes two strings and returns a win or lose message as a string 
+
     const winner = getWinner(player, comp);
     
     if (winner == -1) {
@@ -42,12 +52,14 @@ function playRound(player, comp) {
     
 }
 
-function play(){
+function play(playerChoice){
     // let rounds = 5;
     const choices = ["Rock", "Paper", "Scissors"];
     // for (i = 0; i < rounds; i++) {
     const compChoice = getComputerChoice(choices);
-    const playerChoice = prompt("Rock, Paper, or Scissors: ").trim().toLowerCase();
+    
+    
+    
     console.log(playRound(playerChoice, compChoice));
     // }
 
@@ -55,6 +67,8 @@ function play(){
 }
 
 
+// start game 
 
-play();
-
+rockBtn.addEventListener("click", () => {play('Rock')});
+paperBtn.addEventListener("click", () => {play('Paper')});
+scissorsBtn.addEventListener("click", () => {play('Scissors')});
