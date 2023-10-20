fee9b38fe30fddfa17b055482f99ea8d81469dce
chuka <22582292+Chuka-Obijiaku@users.noreply.github.com>
2023-10-20 00:28:27 -0500

    Seperate play function to play(), playRound()
    the game plays 5 times before exiting app

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function getComputerChoice(possibleChoices) {
-    // Chooses random value from a list of strings
-    return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
+    // Chooses random value from a list of strings and makes it lower case
+    return possibleChoices[Math.floor(Math.random() * possibleChoices.length)].toLowerCase();
 }
 
 function getWinner(player, computer) {
@@ -11,8 +11,11 @@ function getWinner(player, computer) {
     else if (player  == "paper" && computer  == "rock") {
         return true;
     }
-    else if (player  == "scissor" && computer  == "paper") {
+    else if (player  == "scissors" && computer  == "paper") {
         return true;
+    }
+    else if (player == computer) {
+        return -1;
     }
 
     return false;
@@ -20,21 +23,38 @@ function getWinner(player, computer) {
 }
 
 
-function play() {
-    const choices = ["Rock", "Paper", "Scissors"]
-    let compChoice = getComputerChoice(choices);
-    let playerChoice = prompt("Rock, Paper, or Scissors: ").trim();
-    let winner = getWinner(playerChoice, compChoice)
+function playRound(player, comp) {
+    // takes two strings and returns a win or lose message as a string 
+    const winner = getWinner(player, comp);
     
-    if (winner) {
-        alert("You Win");
+    if (winner == -1) {
+        return "It's a tie! You both picked " + player + ".";
+    }
     
+    else if (winner) {
+        return "You Win " + player + " beats " + comp + ".";
+        
     }
     else {
-        alert("You Lose");
+        return "You Lose " + comp + " beats " +  player + ".";
+    }
+    
+    
+}
+
+function play(){
+    let rounds = 5;
+    const choices = ["Rock", "Paper", "Scissors"];
+    for (i = 0; i < rounds; i++) {
+        const compChoice = getComputerChoice(choices);
+        const playerChoice = prompt("Rock, Paper, or Scissors: ").trim().toLowerCase();
+        console.log(playRound(playerChoice, compChoice));
     }
 
 
 }
 
+
+
 play();
+
